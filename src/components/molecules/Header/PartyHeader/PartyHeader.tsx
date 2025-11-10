1935470ef1d1af29fd5c9fe02c836d8d0ed26464
Mateo Velásquez Morales <matius29002@gmail.com>
2025-11-10 01:27:28 -0500

    feat: add PartyHeader component with styling and structure

---
+++ src/components/molecules/Header/PartyHeader/PartyHeader.tsx
@@ -0,0 +1,23 @@
+import { Icon } from "../../../atoms/Icon"
+import PokerChip from '../../../../assets/images/ficha-de-poker 1.svg'
+import { Button } from "../../../atoms/Button"
+import './PartyHeader.scss'
+import { Avatar } from "../../../atoms/Avatar"
+
+interface Params {
+    size?: 'medium' | 'large'
+    avatarText?: string
+    havesAvatar?: boolean
+}
+
+export const PartyHeader = ({size = 'medium', avatarText='', havesAvatar=false}: Params) => {
+    return (
+        <div className="party-container">
+            <Icon src={PokerChip} size={size === 'medium' ? 48 : 70} />
+            <div className="share-party">
+                {havesAvatar ? (<Avatar text={avatarText}/>) : (<></>)}
+                <Button type='share' label='Invitar jugadores'/>
+            </div>
+        </div>
+    )
+}
