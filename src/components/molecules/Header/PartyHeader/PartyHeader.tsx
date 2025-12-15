469e508cd8087be2ac48db12844c83b93f45813c
Mateo Velásquez Morales <matius29002@gmail.com>
2025-12-14 21:47:56 -0500

    feat: add ShareModal component for inviting players and manage shared state in PartyLayoutContainer

--- src/components/molecules/Header/PartyHeader/PartyHeader.tsx
+++ src/components/molecules/Header/PartyHeader/PartyHeader.tsx
@@ -8,15 +8,16 @@ interface Params {
     size?: 'medium' | 'large'
     avatarText?: string
     havesAvatar?: boolean
+    handleOpenSharedModal?: () => void
 }
 
-export const PartyHeader = ({size = 'medium', avatarText='', havesAvatar=false}: Params) => {
+export const PartyHeader = ({size = 'medium', avatarText='', havesAvatar=false, handleOpenSharedModal}: Params) => {
     return (
         <div className="party-container">
             <Icon src={PokerChip} size={size === 'medium' ? 48 : 70} />
             <div className="share-party">
                 {havesAvatar ? (<Avatar text={avatarText} hasBottomLabel={false}/>) : (<></>)}
-                <Button type='share' label='Invitar jugadores'/>
+                <Button type='share' label='Invitar jugadores' parentMethod={handleOpenSharedModal}/>
             </div>
         </div>
     )
--- src/components/organisms/PartyLayoutContainer/PartyLayoutContainer.tsx
+++ src/components/organisms/PartyLayoutContainer/PartyLayoutContainer.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react"
 import { PartyFooter } from "../../molecules/Footer"
 import { PartyHeader } from "../../molecules/Header"
 import { NestedPlanningTable } from "../NestedPlanningTable"
+import { ShareModal } from "../ShareModal"
 import './PartyLayoutContainer.scss'
 
 interface Player {
@@ -27,12 +29,21 @@ export const PartyLayoutContainer = ({
     havesAvatar=false,
     players = []
 }: Params) => {
+    const [isSharedModalOpen, setIsSharedModalOpen] = useState(false)
+
+    const handleClose = () => {
+        setIsSharedModalOpen(false)
+    }
+
+    const hableOpenSharedModal = () => {
+        setIsSharedModalOpen(true)
+    }
     return (
         <div className="party-layout">
             {withHeader ? (
                 <div className="party-layout__header">
                     <label className="party-layout__label">{partyName}</label>
-                    <PartyHeader avatarText={avatarText} havesAvatar={havesAvatar}/>
+                    <PartyHeader avatarText={avatarText} havesAvatar={havesAvatar} handleOpenSharedModal={hableOpenSharedModal}/>
                 </div>
             ) : (<></>)}
             <main className="party-layout__content">
@@ -43,6 +54,8 @@ export const PartyLayoutContainer = ({
                     <PartyFooter />
                 </footer>
             ) : (<></>)}
+
+            <ShareModal isOpen={isSharedModalOpen} handleClose={handleClose}/>
         </div>
     )
 }
